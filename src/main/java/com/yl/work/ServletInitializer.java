632c2917c6e87e83e07389c0a1d7d41e1c822796
yanglu@17ixue.com <yanglu56@17ixue.com>
2019-03-24 17:23:09 +0800

    update

--- src/main/java/com/yl/work/ServletInitializer.java
+++ src/main/java/com/yl/work/ServletInitializer.java
@@ -11,11 +11,11 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(WorkApplication.class);
+        return application.sources(ServletInitializer.class);
     }
 
     public static void main(String[] args) {
-        SpringApplication.run(WorkApplication.class, args);
+        SpringApplication.run(ServletInitializer.class, args);
     }
 
 }
