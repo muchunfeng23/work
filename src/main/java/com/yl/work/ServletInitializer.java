e45175e41cd42fa62be27aea179a8b4a56e62b9b
yanglu@17ixue.com <yanglu56@17ixue.com>
2019-03-31 13:42:54 +0800

    update

--- src/main/java/com/yl/work/ServletInitializer.java
+++ src/main/java/com/yl/work/ServletInitializer.java
@@ -1,22 +1,13 @@
 package com.yl.work;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-//import org.springframework.boot.web.support.SpringBootServletInitializer;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
-//@SpringBootApplication
 public class ServletInitializer extends SpringBootServletInitializer {
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(ServletInitializer.class);
     }
-
-//    public static void main(String[] args) {
-//        SpringApplication.run(ServletInitializer.class, args);
-//    }
 
 }
