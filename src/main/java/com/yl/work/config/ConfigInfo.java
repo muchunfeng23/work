b92a1e34153b3130e7ca88cb40cecc6ec95093a0
yanglu@17ixue.com <yanglu56@17ixue.com>
2019-04-06 12:37:35 +0800

    APPID,SECRET_KEY修改

--- src/main/java/com/yl/work/config/ConfigInfo.java
+++ src/main/java/com/yl/work/config/ConfigInfo.java
@@ -1,8 +1,8 @@
 package com.yl.work.config;
 
 public class ConfigInfo {
-    public static final String APP_ID = "wx97e02b5fde7c34e6";
-    public static final String APP_SECRET = "25f8c78613a0a69f3970f4d89266453d";
+    public static final String APP_ID = "wx71d957bf6d83f2de";
+    public static final String APP_SECRET = "f2a6b2c70bd96d8d4782cf84efa2a0cd";
 
     public static final String THIRD_SESSION = "thirdSession";
     public static final String ALL_ARTICLE = "|allArticles";
