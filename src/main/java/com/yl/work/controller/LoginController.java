163c3761fea27a9d50781594c0bf0a29689dc6d9
yanglu@17ixue.com <yanglu56@17ixue.com>
2019-04-06 12:09:52 +0800

    update

--- src/main/java/com/yl/work/controller/LoginController.java
+++ src/main/java/com/yl/work/controller/LoginController.java
@@ -30,7 +30,7 @@ public class LoginController {
     @RequestMapping("/uploadUserInfo")
     public String testServer(HttpServletRequest request, @RequestParam("uploadUserInfo")String uploadUserInfo,@RequestParam("user_token")String userToken){
         logger.info("user_token = " + userToken);
-        String sessionKey = LoginHelper.getOpenIdAndSessionKey(userToken).getSession_key();
+        String sessionKey = LoginHelper.getOpenIdFromContext(userToken).getSession_key();
         logger.info("sessionKey = " + sessionKey);
         wxCredentialUserHelper.uploadLoginUserCredentialData(uploadUserInfo,sessionKey);
         System.out.println("-----  " + uploadUserInfo);
