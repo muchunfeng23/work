47f407193de2e0e61ee587c88638ee999bbde911
yanglu <yanglu04@meicai.cn>
2020-03-08 16:33:54 +0800

    update

--- src/main/java/com/yl/work/dto/ConceptChangeData.java
+++ src/main/java/com/yl/work/dto/ConceptChangeData.java
@@ -7,10 +7,28 @@ public class ConceptChangeData implements Serializable {
     private String conceptName;
     private String conceptDayIndex;
     private String paihangIndex;
+    private String zhangdiefu;
+    private String sumMoney;
     //当日排行越高分值越大；当日没进前N，不在排行之内
     private List<Integer> scoreList;
     private Integer score;
     private List<Double> percentList;
+
+    public String getZhangdiefu() {
+        return zhangdiefu;
+    }
+
+    public void setZhangdiefu(String zhangdiefu) {
+        this.zhangdiefu = zhangdiefu;
+    }
+
+    public String getSumMoney() {
+        return sumMoney;
+    }
+
+    public void setSumMoney(String sumMoney) {
+        this.sumMoney = sumMoney;
+    }
 
     public List<Double> getPercentList() {
         return percentList;
